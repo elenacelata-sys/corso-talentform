08889fc96a6aeede3087e54e45f4c0491099493d
elenacelata <elena.celata@gmail.com>
2025-10-30 13:52:15 +0100

    fix react

--- TYPESCRIPT/modules/10.intersection-types/intersection-types.js
+++
@@ -1,7 +0,0 @@
-var employe = {
-    name: 'Max',
-    employe: 'Technical Lead',
-    privileges: ['Coordinator Team'],
-    startDate: new Date()
-};
-console.log(employe);
--- TYPESCRIPT/modules/4.objects/objects.js
+++
@@ -1,8 +0,0 @@
-const nation = {
-    id: 1,
-    name: "italy",
-    city: "turin",
-};
-console.log(nation);
-console.log(nation.name);
-//# sourceMappingURL=objects.js.map
